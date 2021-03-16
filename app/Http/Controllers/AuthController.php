7200735f07d5296773bfdc787caacb99b54f8e69
Muhammad Zeeshan Shafqat <zeeshan_github@outlook.com>
2021-03-17 00:44:49 +1100

    added auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller {
+
+    //register function
+
+    //login function
+
+    //logout function
+
+    //get authenticated user
+    
+}
