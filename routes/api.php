50f3a601db5571b673c0e568887002d40ff5ab6f
Muhammad Zeeshan Shafqat <zeeshan_github@outlook.com>
2021-03-17 17:23:59 +1100

    added auth routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
+
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
@@ -19,5 +21,11 @@ Route::get('/user', [UserController::class, 'show']);
 Route::post('/post', [PostController::class, 'store']);
 Route::get('/post', [PostController::class, 'show']);
 Route::get('/posts', [PostController::class, 'showAllForUser']);
+
+//Authentication
+Route::post('/auth/signup', [AuthController::class, 'signup']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->get('/auth/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:api')->get('/auth/user',  [AuthController::class, 'user']);
 
 
